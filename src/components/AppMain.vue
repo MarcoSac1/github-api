338c7dc46be171064ebd9b18e8a75ee6e65b0830
MarcoSac1 <marcosaccone8@gmail.com>
2024-10-10 00:23:09 +0200

    First commit

---
+++ src/components/AppMain.vue
@@ -0,0 +1,80 @@
+<script>
+import AppSearch from './AppSearch.vue';
+import CardList from './CardList.vue';
+import axios from 'axios';
+export default{
+    components: {
+        CardList,
+        AppSearch
+    },
+    data(){
+        return{
+            tipologiaSelected: null,
+            cards:[],
+            tipiRicerca: [
+                {
+                    nome: 'utenti',
+                    descrizione: 'Utenti/Organizzazioni'
+                },
+                {
+                    nome: 'repo',
+                    descrizione: 'Repository'
+                }
+            ]
+        }
+    },
+    methods:{
+        onTipologiaSelected(tipologia) {
+            this.tipologiaSelected = tipologia;
+        },
+        onQuerySearch(tipologia, query) {
+            console.log('tipologia', tipologia)
+            console.log('query', query)
+
+            // In base alla tipologia chiamo searchUtenti o searchRepo
+        },
+        searchUtenti(query){
+            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0&archetype=' + pippo)
+            .then((response) => {
+                console.log(response.items);
+                this.cards=response.items;
+            })
+            .catch(function(error){
+                console.log(error);
+            })
+            .finally(function () {
+                
+            });
+        },
+        searchRepo(query){
+            axios.get('https://db.ygoprodeck.com/api/v7/archetypes.php')
+            .then((response) => {
+                console.log(response.items);
+                this.cards=response.items;
+                console.log(this.tipiRicerca);
+            })
+            .catch(function(error){
+                console.log(error);
+            })
+            .finally(function () {
+                
+            });
+        },
+
+    },
+    created(){
+    }
+
+}
+</script>
+
+<template>
+    <main>
+        <AppSearch @selected='onTipologiaSelected' @searched="onQuerySearch" :tipiRicerca="tipiRicerca"/>
+        <CardList :cards="cards"/>
+    </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,54 @@
+<script>
+export default{
+    components: {
+        
+    },
+    data(){
+        return{
+            cercato:'',
+            cercatoDesc: 'Tipologia Ricerca',
+            queryRicerca: ''
+        }
+    },
+    props:{
+        tipiRicerca:{
+            type: Array,
+            required: true
+        }
+    }
+    
+}
+</script>
+
+<template >
+    
+    <section class="container mb-4 mt-4">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="dropdown">
+                    <button class="btn btn-danger dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" >
+                        {{  cercatoDesc }}
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-dark">
+                        <li  v-for="(tipiRicerca, index) in tipiRicerca" :key="index" :tipiRicerca="tipiRicerca"  >
+                            <a class="dropdown-item " @click="cercato = tipiRicerca.nome;cercatoDesc = tipiRicerca.descrizione"  href="#">{{ tipiRicerca.descrizione }}</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6" v-if="cercato">
+                    <input class="form-control" v-model="queryRicerca" type="search" placeholder="Search" aria-label="Cerca">
+            </div>
+            <div class="col-md-2" v-if="cercato">
+                <button class="btn btn-outline-success" @click="$emit('searched', cercato, queryRicerca)" type="submit">Cerca</button>
+            </div>
+        </div>
+    </section>
+</template>
+<!-- v-model="cercato"  -->
+<style scoped>
+    .dropdown-menu {
+        max-height: 500px;
+        overflow-y: auto;
+    }
+</style>
